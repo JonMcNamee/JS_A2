4bd517e52a237d88004dfd301391fec634876235
JonMcNamee <117097013+JonMcNamee@users.noreply.github.com>
2023-01-31 20:01:45 -0400

    Update A2.js

--- A2.js
+++ A2.js
@@ -1,36 +1,52 @@
+
 window.onload = function () 
 {
     document.querySelector("#tabContainer");
+
     document.addEventListener("click", TabClick);
+
     document.querySelectorAll(".tab")[0].click();
-};
-
-
-function TabClick(evt) 
-{
-    let tab = evt.target;
-    deselectTab();
-    tab.classList.add("active");
-    hideSection();
-    let id = tab.innerHTML; 
-    id = id.replace(" ", "_");
-    document.querySelector("#" + id).classList.remove("hidden");
 }
 
+//function to trigger class changes on click of new tab
+function TabClick(evt) 
+{
+
+    //sets clicked tab as active 
+    let tab = evt.target;
+
+    //deselect before adding active, else nothing shows up
+    deselectTab();
+    tab.classList.add("active");
+
+   // process for swapping active and hidden tabs
+    let id = tab.innerHTML; 
+    id = id.replace(" ", "_"); 
+
+    hideSection();
+
+    document.querySelector("#" + id).classList.remove("hidden"); 
+}
+
+//function to add hidden class to inactive tabs, so that only selected tab content will appear
+function hideSection() 
+{
+    let sections = document.querySelectorAll(".tabContent");
+
+    for (let i = 0; i < sections.length; i++) 
+    {
+        sections[i].classList.add("hidden");
+    }
+}
+
+// function to remove active class from tab 
 function deselectTab() 
 {
     let tabs = document.querySelectorAll(".tab");
+    
     for (let i = 0; i < tabs.length; i++) 
     {
         tabs[i].classList.remove("active");
     }
 }
 
-function hideSection() 
-{
-    let sections = document.querySelectorAll(".tabContent");
-    for (let i = 0; i < sections.length; i++) 
-    {
-        sections[i].classList.add("hidden");
-    }
-}
