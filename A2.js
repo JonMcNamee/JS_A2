4c201314076d629a1f6b5eb536f57bd7ad5a33ec
JonMcNamee <117097013+JonMcNamee@users.noreply.github.com>
2023-02-03 12:51:55 -0400

    Update A2.js

--- A2.js
+++ A2.js
@@ -8,8 +8,18 @@ window.onload = function ()
     let btnT2 = document.querySelector("#btnT2");
     btnT2.addEventListener("click", T2Calc);
 
-    let btn4 = document.querySelector("#btnT4");
+    let btnT4 = document.querySelector("#btnT4");
     btnT4.addEventListener("click", T4Calc);
+    
+    let btnT6 = document.querySelector("#btnT6");
+    btnT6.addEventListener("click", T6Calc);
+
+    let btnT8 = document.querySelector("#btnT8");
+    btnT8.addEventListener("click", T8Calc)
+
+    let btnT10 = document.querySelector("#btnT10");
+    btnT10.addEventListener("click", T10Calc);
+
 }
 
 //function to trigger class changes on click of new tab
@@ -119,4 +129,105 @@ function T4Calc()
 
     let out = document.querySelector("#T4OutText");
     out.innerHTML = output;
+}
+
+/*Asks the user for a string statement, which are then counted to display total number and new string*/
+function T6Calc()
+{
+    let vowels = "AEIOUaeiou";
+
+    let input = document.querySelector("#T6Input").value;
+    let output = "";
+
+    let count = 0;
+
+    //for loop to parse through inputted string and change vowels, and increment count
+    for(let i = 0; i < input.length; i++)
+    {
+        if(vowels.indexOf(input[i]) !== -1)
+        {
+            output+= "*";
+            count++;
+        }
+        else
+        {
+            output += input[i];
+        }
+    }
+
+    console.log(output);
+    console.log(count);
+    let out = document.querySelector("#T6OutText");
+    out.innerHTML = "<p> Number of Vowels: " + count + "<br><br>"+"New String: " + output + "</p>";
+
+}
+
+function T8Calc()
+{
+    let value = Number(document.querySelector("#T8Input").value)
+
+    let singleValue = 1.75;
+    let boxValue = 32;
+
+    let boxTotal = Math.floor(value / 24);
+    let barTotal = value % 24;
+
+    let boxCost = boxTotal * boxValue;
+    let barCost = barTotal * singleValue;
+
+    let total = boxCost + barCost;
+
+    let output = "";
+
+    if(value < 1)
+    {
+        output += "ERROR. INPUT MUST BE 1 OR MORE";
+    }
+    else
+    {
+        output += "<p>Number of boxes: " + boxTotal + "<br>" + "Number of Singles: " + barTotal + "<br>" + "Cost of Boxes: $" + boxCost
+        + "<br>" + "Cost of Singles: $" + barCost + "<br>" + "Total Cost: $" + total + "</p>";
+    }
+
+    let out = document.querySelector("#T8OutText")
+    out.innerHTML = output;
+}
+
+function T10Calc()
+{
+    let angleA = Number(document.querySelector("#T10Input1").value);
+    let angleB = Number(document.querySelector("#T10Input2").value);
+    let angleC = Number(document.querySelector("#T10Input3").value);
+
+    let total = angleA + angleB + angleC;
+
+    let output = "";
+
+    //if statement to confirm angles are valid
+    if(total > 180 || total < 3)
+    {
+        output += "<p>ERROR: INVALID TRIANGLE. ANGLES CANNOT EXCEED 180 DEGREES AND MUST BE GREATER THAN 0</p>"
+    }
+    else
+    {
+        //nested if else chain to apply correct output result based on angle inputs
+        if(angleA === 90 || angleB === 90 || angleC === 90)
+        {
+            output+= "<p>YOUR TRIANGLE IS A RIGHT TRIANGLE</p>";
+        }
+        else if(angleA > 90 || angleB > 90 || angleC > 90)
+        {
+            output += "<p>YOUR TRIANGLE IS AN OBTUSE TRIANGLE</p>";
+        }
+        else
+        {
+            output += "<p>YOUR TRIANGLE IS AN ACUTE TRIANGLE</p>";
+        }
+    }
+    
+    console.log(total);
+
+    let out = document.querySelector("#T10OutText");
+    out.innerHTML= output;
+
 }
